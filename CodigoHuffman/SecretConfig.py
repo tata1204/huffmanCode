eec0c1e01c3b9e117ea2ddaaac1465f40b6a9cdf
tata1204 <147460498+tata1204@users.noreply.github.com>
2024-05-15 00:51:04 -0500

    Add files via upload

---
+++ CodigoHuffman/SecretConfig.py
@@ -0,0 +1,9 @@
+# Recuerde poner aqui los datos d econexion y guardar como SecretConfig.py
+
+# Do not expose your Neon credentials to the browser
+
+PGHOST='ep-autumn-lake-a51htc5i.us-east-2.aws.neon.tech'
+PGDATABASE='HuffmanDataBase'
+PGUSER='tarjeta de credito_owner'
+PGPASSWORD='QyaEWtdGn7I5'
+PGPORT=5432
